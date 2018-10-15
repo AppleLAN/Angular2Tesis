4c6f0d349ba1df413d4f5a42f11f11011e0ca1da
AlanBuscaglia <alanbuscaglia@gmail.com>
2018-10-15 16:08:55 -0300

    nueva manera de enviar la informacion a los endpoints de ordenes de compra

--- src/app/appsModule/sales/services/sale.service.ts
+++ src/app/appsModule/sales/services/sale.service.ts
@@ -1,4 +1,9 @@
-import { CHANGEORDER, DELETEORDER, NEWORDERS, OrdersState } from '../reducers/order.reducer';
+import {
+  CHANGEORDER,
+  DELETEORDER,
+  NEWORDERS,
+  OrdersState
+} from '../reducers/order.reducer';
 import { NEWSALES, SaleState } from '../reducers/sale.reducer';
 
 import { Order } from '../../../interfaces/order';
@@ -20,68 +25,82 @@ export class SaleService {
   constructor(private api: Http, private store: Store<OrdersState>) {
     this.orderStorage = store.select('orders');
     this.saleStorage = store.select('sales');
-}
+  }
 
-    buy(addedProducts: NewBuy): Observable<any> {
-        return this.api.post('https://contaduriabackend.herokuapp.com/api/saveBuyOrder', addedProducts)
-            .map((response: Response) => {
-                return response.json();
-            })
-            .catch((error: any) => Observable.throw(error || 'Server error'));
-    }
+  buy(addedProducts: NewBuy): Observable<any> {
+    return this.api
+      .post(
+        'https://contaduriabackend.herokuapp.com/api/saveBuyOrder',
+        addedProducts
+      )
+      .map((response: Response) => {
+        return response.json();
+      })
+      .catch((error: any) => Observable.throw(error || 'Server error'));
+  }
 
-    sale(addedProducts: NewSale): Observable<any> {
-        return this.api.post('https://contaduriabackend.herokuapp.com/api/createNewSale', addedProducts)
-            .map((response: Response) => {
-                return response.json();
-            })
-            .catch((error: any) => Observable.throw(error || 'Server error'));
-    }
+  sale(addedProducts: NewSale): Observable<any> {
+    return this.api
+      .post(
+        'https://contaduriabackend.herokuapp.com/api/createNewSale',
+        addedProducts
+      )
+      .map((response: Response) => {
+        return response.json();
+      })
+      .catch((error: any) => Observable.throw(error || 'Server error'));
+  }
 
-    getSaleStorage(): Observable<SaleState> {
-        return this.saleStorage;
-    }
+  getSaleStorage(): Observable<SaleState> {
+    return this.saleStorage;
+  }
 
-    getOrderStorage(): Observable<OrdersState> {
-        return this.orderStorage;
-    }
+  getOrderStorage(): Observable<OrdersState> {
+    return this.orderStorage;
+  }
 
-    getAllOrders(): Observable<any> {
-        return this.api.get('https://contaduriabackend.herokuapp.com/api/getBuyOrders')
-        .map((response: Response) => {
-            this.store.dispatch({ type: NEWORDERS, payload: response.json()});
-            return response.json();
-        })
-        .catch((error: any) => Observable.throw(error || 'Server error'));
-    }
+  getAllOrders(): Observable<any> {
+    return this.api
+      .get('https://contaduriabackend.herokuapp.com/api/getBuyOrders')
+      .map((response: Response) => {
+        this.store.dispatch({ type: NEWORDERS, payload: response.json() });
+        return response.json();
+      })
+      .catch((error: any) => Observable.throw(error || 'Server error'));
+  }
 
-    getAllSales(): Observable<any> {
-        return this.api.get('https://contaduriabackend.herokuapp.com/api/saleInformation')
-        .map((response: Response) => {
-            this.store.dispatch({ type: NEWSALES, payload: response.json()});
-            return response.json();
-        })
-        .catch((error: any) => Observable.throw(error || 'Server error'));
-    }
+  getAllSales(): Observable<any> {
+    return this.api
+      .get('https://contaduriabackend.herokuapp.com/api/saleInformation')
+      .map((response: Response) => {
+        this.store.dispatch({ type: NEWSALES, payload: response.json() });
+        return response.json();
+      })
+      .catch((error: any) => Observable.throw(error || 'Server error'));
+  }
 
-    deleteOrder(order: Order): Observable<any> {
-        return this.api.post(`https://contaduriabackend.herokuapp.com/api/deleteOrder/`, order.id)
-        .map((response: Response) => {
-            this.store.dispatch({ type: DELETEORDER, payload: order});
-            return response.json();
-        })
-        .catch((error: any) => Observable.throw(error || 'Server error'));
-    }
+  deleteOrder(order: Order): Observable<any> {
+    return this.api
+      .post(`https://contaduriabackend.herokuapp.com/api/deleteOrder`, {
+        id: order.id
+      })
+      .map((response: Response) => {
+        this.store.dispatch({ type: DELETEORDER, payload: order });
+        return response.json();
+      })
+      .catch((error: any) => Observable.throw(error || 'Server error'));
+  }
 
-    completeOrder(order: Order): Observable<any> {
-        return this.api.post(`https://contaduriabackend.herokuapp.com/api/completeOrder/`, order.id)
-        .map((response: Response) => {
-            order.status = 'R';
-            this.store.dispatch({ type: CHANGEORDER, payload: order});
-            return response.json();
-        })
-        .catch((error: any) => Observable.throw(error || 'Server error'));
-    }
-
-
+  completeOrder(order: Order): Observable<any> {
+    return this.api
+      .post(`https://contaduriabackend.herokuapp.com/api/completeOrder`, {
+        id: order.id
+      })
+      .map((response: Response) => {
+        order.status = 'R';
+        this.store.dispatch({ type: CHANGEORDER, payload: order });
+        return response.json();
+      })
+      .catch((error: any) => Observable.throw(error || 'Server error'));
+  }
 }
